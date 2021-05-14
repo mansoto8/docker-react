65142bcd264d4ebfeb11b89fdee05ae9fb1df209
juan <mansoto8@gmail.com>
2021-05-14 08:56:46 -0500

    added change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hi Hey cool
+          Edit <code>src/App.js</code> and save to reload. Hi Hey cool. I was change in feature.
         </p>
         <a
           className="App-link"
